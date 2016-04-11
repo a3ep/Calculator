918dc73a0b725b3cb8795f3c9a775954e825334c
Vsevolod Bondar <bondvsev@gmail.com>
2016-04-11 10:32:37 +0300

    Added comments for NumberBuilder.

--- src/main/java/net/bondar/interfaces/INumberBuilder.java
+++ src/main/java/net/bondar/interfaces/INumberBuilder.java
@@ -1,22 +1,24 @@
 package net.bondar.interfaces;
 
 /**
- *
+ * Interface for class that provides building the parts of expression.
  */
 public interface INumberBuilder {
+
     /**
+     * Converts the received string with a number value to the number.
      *
-     * @param stringNumber
-     * @return
+     * @param stringNumber the string to be converted to the number
+     * @return number value
      */
     int buildNumber(String stringNumber);
 
     /**
+     * Builds a string with a number value based on the specified substring.
      *
-     * @param str
-     * @param expression
-     * @return
+     * @param str        specified substring
+     * @param expression string with expression
+     * @return string with a number value
      */
     String buildStringNumber(String str, String expression);
-
 }
--- src/main/java/net/bondar/utils/NumberBuilder.java
+++ src/main/java/net/bondar/utils/NumberBuilder.java
@@ -5,30 +5,41 @@ import net.bondar.interfaces.INumberBuilder;
 import org.apache.log4j.Logger;
 
 /**
- *
+ * Provides building the parts of expression.
  */
 public class NumberBuilder implements INumberBuilder {
+
+    /**
+     * Logger.
+     */
     private final static Logger log = Logger.getLogger(NumberBuilder.class);
 
     /**
-     * @param stringNumber
-     * @return
+     * Converts the received string with a number value to the number.
+     *
+     * @param stringNumber the string to be converted to the number
+     * @return number value
+     * @throws CalculatorApplicationException when the string with a number value has incorrect format
+     * @see {@link INumberBuilder}
      */
     public int buildNumber(String stringNumber) {
         log.info("---------- Builds number...");
         if (stringNumber.length() != 0) {
             try {
                 return Integer.parseInt(stringNumber);
-            }catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 throw new CalculatorApplicationException(e.getMessage());
             }
         }
-        throw new CalculatorApplicationException("Error while building number -->"+stringNumber);
+        throw new CalculatorApplicationException("Error while building number -->" + stringNumber);
     }
 
     /**
-     * @param str
-     * @return
+     * Builds a string with a number value based on the specified substring.
+     *
+     * @param str        specified substring
+     * @param expression string with expression
+     * @return string with a number value or empty string, if the specified substring is empty
      */
     public String buildStringNumber(String str, String expression) {
         log.info("---------- Builds part of expression...");
@@ -38,13 +49,19 @@ public class NumberBuilder implements INumberBuilder {
         if (expression.indexOf(str) == 0) {
             StringBuilder builder = new StringBuilder(str);
             str = builder.reverse().toString();
-            str = reverseOperator(str);
+            str = findEndOfNumber(str);
             return builder.replace(0, builder.length(), str).reverse().toString();
         }
-        return reverseOperator(str);
+        return findEndOfNumber(str);
     }
 
-    public String reverseOperator(String str) {
+    /**
+     * Finds the end of the number in the specified substring.
+     *
+     * @param str the specified substring
+     * @return string with the number value
+     */
+    private String findEndOfNumber(String str) {
         if (str.length() > 1) {
             char[] arr = str.toCharArray();
             for (int i = 0; i < arr.length; i++) {
